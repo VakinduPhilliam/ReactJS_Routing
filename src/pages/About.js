c1c2a2bed280600dce25e8ad10f398482cc2f20e
Vakindu Philliam <Pvakindu@gmail.com>
2019-07-23 13:56:09 +0300

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+
+function About(props) {
+    
+
+return (
+      
+
+<div className="page-container">
+        
+
+<h1 className="title">About</h1>
+      
+
+</div>
+   
+
+ )
+}
+
+
+
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+
+function Home(props) {
+   
+
+ return (
+      
+
+<div className="page-container">
+        
+
+<h1 className="title">Hello</h1>
+      
+
+</div>
+   
+
+ )
+}
+
+
+
+
+export default Home;
